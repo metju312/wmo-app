1f994308504daa0bc8d314500800cad27623a440
mikolaj.mocarski <mikolaj.mocarski@accenture.com>
2018-03-21 15:42:23 +0100

    Wstepny szkielet widoku listy projektów

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -8,36 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectComponent implements OnInit {
   ngOnInit(): void {
   }
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+
+  displayedColumns = ['name'];
   dataSource = ELEMENT_DATA;
 }
 
 export interface Element {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {name: 'Projekt 1'},
+  {name: 'Projekt 2'},
+  {name: 'Projekt 3'},
+  {name: 'Projekt 4'},
+  {name: 'Projekt 5'},
+  {name: 'Projekt 6'},
+  {name: 'Projekt 7'},
+  {name: 'Projekt 8'},
+  {name: 'Projekt 9'},
+  {name: 'Projekt 10'},
+  {name: 'Projekt 11'},
+  {name: 'Projekt 12'},
+  {name: 'Projekt 13'}
 ];
